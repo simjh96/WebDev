034e8aace4e457010c40287d862c1288cfa82c2f
simjh96 <simjh96@yonsei.ac.kr>
2022-01-24 18:19:10 +0900

    -

--- time_gap/brakets.py
+++ time_gap/brakets.py
@@ -1,11 +1,11 @@
 
 def parsable(g: str)->bool:
-  return sum([int(_) for _ in g]) == len(g)/2
+  return sum([int(_) for _ in g]) == len(g)//2 and (not len(g)%2)
 
 def parse_g(g: str)->list:
   # parse to minimum subset of AB -> A, B
   #   posibility of duplicate interpretation only happens with 'wrap' operation involoved
-  # ['01','10','0011']
+  # ['01','10','0011'] with len > 1
   i, j, zs, os = 0, 0, 0, 0
   result = []
 
@@ -31,17 +31,23 @@ def count_leaf(pg: list):
     #   (if mergable, than successive 'and' relation is passed to next recursion)
     mergable = int(pg[-2][0]) + int(pg[-1][-1]) == 1
     print(mergable)
+    _pg = pg[:]
+    _pg[0], _pg[-1] = pg[0][1:], pg[-1][:-1]
     if mergable:
-      return 2*count_leaf(pg[:-1])   # _unwrapable -unwrap-> _unwrapable with idx 0, -1 switched; count_leaf stay same
+      return count_leaf(pg[:-1]) + count_leaf(parse_g(''.join(_pg)))   # _unwrapable -unwrap-> _unwrapable with idx 0, -1 switched; count_leaf stay same
     else:
       return count_leaf(pg[:-1])
   
-  else:
+  elif len(pg) == 1:
     # minimum element has 1 interpretation -> unwrap
-    if len(pg[0]) == 2:
-      return 1
+    g = pg[0]
+    mergable = int(g[0]) + int(g[-1]) == 1
+    if mergable and len(g)>2:
+      return count_leaf(parse_g(g[1:-1]))
     else:
-      return count_leaf(parse_g(pg[0][1:-1]))
+      return 1
+  else:
+    return 1
 
 g = input()
 if parsable(g):
@@ -49,3 +55,5 @@ if parsable(g):
   print(count_leaf(pg)%(10**9+7))
 else:
   print(0)
+
+
